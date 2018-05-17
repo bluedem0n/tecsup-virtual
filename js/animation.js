f7cf1d4e56ecaebcc276bfb1acbe5ea131438e4a
Sthefany Liseth Floriano Guzman <sfloriano@zegelipae.edu.pe>
2018-05-17 15:33:55 -0500

    Video

--- js/animation.js
+++ js/animation.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+    var detector = 0;
     $('header nav li a').hover(function(){
         $('.hover').fadeIn(1);
         $(".hover").offset({top: $(this).offset().top-18, left: $(this).offset().left-10});
@@ -6,19 +7,28 @@ $(document).ready(function(){
     }, function(){
         $('.hover').fadeOut(1);
     });
-     $('.contenedor .noti .izq button').hover(function(){
-        $('.contenedor .noti .izq p').fadeOut(1000);         
-        $(this).fadeOut(1000);
-        $('.contenedor .noti .izq .boton').fadeIn(1000);
-        $('#video').fadeIn(500, function(){
-            $('#video').get(0).play();
-        });
-    });
-    $('.contenedor .noti .izq .boton').click(function(){
-        $('.contenedor .noti .izq p,.contenedor .noti .izq button').fadeIn(1000);
-        $('.contenedor .noti .izq .boton').fadeOut(1000);
-        $('#video').fadeOut(500, function () {
+    $("html").click(function() {
+        if($('.pop').is(":visible")){
+            $('.pop').fadeOut();
             $('#video').get(0).pause();
-        });
+        } 
+    });
+    $('.pop').click(function (e) {
+        e.stopPropagation();
+    });
+    $('.contenedor .noti .izq button').click(function(e){
+        if(!$('.pop').is(":visible")){
+            e.stopPropagation();
+        } 
+        $('.pop').fadeIn();
+        $(this).fadeOut();
+        $('.contenedor .noti .izq button').fadeOut();
+        $('#video').get(0).play();
+        $('.contenedor .noti .izq .pop .boton').fadeIn();
+    });
+    $('.contenedor .noti .izq .pop .boton').click(function(){
+        $('.pop').fadeOut();
+        $('#video').get(0).pause();
+        $('.contenedor .noti .izq button').fadeIn();
     });
 });
