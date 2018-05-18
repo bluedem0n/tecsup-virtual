59cc8e31cda80027279b8ff5d783bef6691f50bb
Sthefany Liseth Floriano Guzman <sfloriano@zegelipae.edu.pe>
2018-05-18 15:47:20 -0500

    Animaciones

--- js/animation.js
+++ js/animation.js
@@ -1,6 +1,12 @@
 $(document).ready(function(){
     var detector = 0;
     $('.hover').fadeOut(1);
+    window.sr = ScrollReveal({ reset: true });
+
+    // Customizing a reveal set
+    sr.reveal('.foo', { duration: 700 });
+    sr.reveal('.padre');
+    sr.reveal('.padre');
     $('header nav li a').hover(function(){
         $('.hover').fadeIn(1);
         $(".hover").offset({top: $(this).offset().top-25, left: $(this).offset().left-10});
@@ -14,8 +20,20 @@ $(document).ready(function(){
             $('#video').get(0).pause();
             $('.contenedor .noti .izq button').fadeIn();
             $('.oscuro').fadeOut(500);
+            $('.contenedor .noti .izq').fadeIn();
         } 
     });
+
+    $('.mes .contenedor .padre .caja').hover(function(){
+        $(".der", this).css("background", "#a23892");
+        $(".izq .title", this).css("color", "#a23892");
+
+    }, function(){
+        $(".der", this).css("background", "#01aef0");
+        $(".izq .title", this).css("color", "#01aaf0");
+        
+    });
+
     $('.pop').click(function (e) {
         e.stopPropagation();
     });
@@ -23,19 +41,25 @@ $(document).ready(function(){
         if(!$('.pop').is(":visible")){
             e.stopPropagation();
         } 
-        $('.pop').fadeIn();
+        $('.pop, .pop .boton').fadeIn();
         $(this).fadeOut();
-        $('.contenedor .noti .izq button').fadeOut();
+        $('.contenedor .noti .izq').fadeOut();
+
         $('#video').get(0).play();
-        $('.contenedor .noti .izq .pop .boton').fadeIn();
+        $('.pop .boton').fadeIn();
         $('.oscuro').fadeIn(500);
         $('html, body').stop(true).animate({
-            scrollTop: ($('.pop').offset().top -200)
+            scrollTop: ($('.pop').offset().top-500)
         }, 500);   });
-    $('.contenedor .noti .izq .pop .boton').click(function(){
+
+
+
+    $('.pop .boton').click(function(){
         $('.pop').fadeOut();
         $('#video').get(0).pause();
-        $('.contenedor .noti .izq button').fadeIn();
+        $('.contenedor .noti .izq').fadeIn();
         $('.oscuro').fadeOut(500);
+        $(this).fadeOut();
+        $('.contenedor .noti .izq').fadeIn();
     });
 });
